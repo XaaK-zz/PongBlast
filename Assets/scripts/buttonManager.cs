0e9451c288c410ee6af6d3a04c6cb9ebaee6aeb3
bouncy87 <dgreenvoss@gmail.com>
2020-03-29 19:09:14 -0700

    add tmp

---
+++ Assets/scripts/buttonManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class buttonManager : MonoBehaviour
+{
+    public void OnButton()
+    {
+        Debug.Log("Button was pressed!");
+    }
+
+}
