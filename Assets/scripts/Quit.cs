0ffb88db24c251ed9ef371010594578436182c81
bouncy87 <dgreenvoss@gmail.com>
2020-03-30 20:35:43 -0700

    basic main menu

---
+++ Assets/scripts/Quit.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void doquit()
+    {
+        Debug.Log("quitting");
+        Application.Quit();
+
+    }
+
+
+}
+
---
+++ Assets/scripts/SceneLoader.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+using System.Collections;
+
+public class SceneLoader : MonoBehaviour
+{
+
+    public void LoadScene(int level)
+    {
+        Application.LoadLevel(level);
+    }
+}
