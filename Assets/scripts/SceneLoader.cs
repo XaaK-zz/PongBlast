af895eb2a6cdd5ccb70271cb034b98d42889b85d
bouncy87 <dgreenvoss@gmail.com>
2020-03-31 09:36:10 -0700

    added glitch and title

--- Assets/scripts/SceneLoader.cs
+++ Assets/scripts/SceneLoader.cs
@@ -2,12 +2,52 @@
 using UnityEditor;
 using System.Collections.Generic;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-
-    public void LoadScene(int level)
+    public ponganimation animate;
+   
+    public bool done;
+    public void Start()
     {
-        Application.LoadLevel(level);
+        GameObject g = GameObject.FindGameObjectWithTag("pong");
+
+        animate = g.GetComponent<ponganimation>();
+       
+    }
+    public void buttonPressed()
+    {
+        animate.pressed = true;
+        
+        StartCoroutine(Coroutine());
+       
+    }
+    public void LoadScene()
+    {
+        Debug.Log("loaded");
+        SceneManager.LoadScene(sceneBuildIndex:1);
+    }
+         
+    
+        
+        
+        
+    
+    public void Update()
+    {
+        if (done == true)
+        {
+            Debug.Log("loading");
+            LoadScene();
+            
+
+        }
+    }
+    IEnumerator Coroutine()
+    {
+        yield return new WaitForSeconds(4);
+        done = true;
+        Debug.Log("done");
     }
 }
---
+++ Assets/scripts/blastbutton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blastbutton : MonoBehaviour
+{
+    public blast animate;
+   
+    void Start()
+    {
+        GameObject g = GameObject.FindGameObjectWithTag("blast");
+
+        animate = g.GetComponent<blast>();
+    }
+
+    public void onButton()
+    {
+        animate.pressed = true;
+    }
+}
